7d9177079ecb0109c561ec681801fe76c40d11c9
Ariman <arimanecro@gmail.com>
2023-04-07 15:51:10 +0300

    Update main.js

--- pc/template-page/assets/js/main.js
+++ pc/template-page/assets/js/main.js
@@ -394,29 +394,29 @@
     });
 
 
-    $(window).on('load', function () {
+//     $(window).on('load', function () {
 
-        /*-----------------
-            preloader
-        ------------------*/
-        var preLoder = $("#preloader");
-        preLoder.fadeOut(0);
+//         /*-----------------
+//             preloader
+//         ------------------*/
+//         var preLoder = $("#preloader");
+//         preLoder.fadeOut(0);
 
-        /*-----------------
-            back to top
-        ------------------*/
-        var backtoTop = $('.back-to-top')
-        backtoTop.fadeOut();
+//         /*-----------------
+//             back to top
+//         ------------------*/
+//         var backtoTop = $('.back-to-top')
+//         backtoTop.fadeOut();
 
-        /*---------------------
-            Cancel Preloader
-        ----------------------*/
-        $(document).on('click', '.cancel-preloader a', function (e) {
-            e.preventDefault();
-            $("#preloader").fadeOut(2000);
-        });
+//         /*---------------------
+//             Cancel Preloader
+//         ----------------------*/
+//         $(document).on('click', '.cancel-preloader a', function (e) {
+//             e.preventDefault();
+//             $("#preloader").fadeOut(2000);
+//         });
 
-    });
+//     });
 
 
 
